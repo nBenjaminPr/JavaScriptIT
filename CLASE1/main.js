71c57379423976b77769120b0c3c236ab7b95406
Nicolas <nicobenpe@gmail.com>
2023-12-02 13:13:32 -0300

    objetos

--- CLASE1/main.js
+++ CLASE1/main.js
@@ -8,3 +8,21 @@ let string = "Esto es un texto o string"
 string = "Es otro texto"
 
 console.log (string)
+
+console.log(2 + 2 )
+
+console.log (2 + "2")
+
+let number = 356
+
+console.log (typeof number)
+number = "pepito"
+
+
+let objeto = { nombre: "jorge", apellido: "pedro"}
+
+console.log(objeto)
+
+
+console.log(2 + 3);
+console.log(2 * 3);
