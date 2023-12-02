d06893893b9ad0e410ff9bf09e1c62c7577c35b7
Nicolas <nicobenpe@gmail.com>
2023-12-02 12:02:17 -0300

    Variables

--- CLASE1/main.js
+++ CLASE1/main.js
@@ -1,3 +1,10 @@
 console.log("Mensaje para la consola")
 console.error("Todo mal")
 console.warn("Micho")
+
+
+let string = "Esto es un texto o string"
+
+string = "Es otro texto"
+
+console.log (string)
