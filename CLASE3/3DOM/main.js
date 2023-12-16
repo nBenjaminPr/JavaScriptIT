4547f294d1a177be3c6d6391e3367831ce39a443
Nicolas <nicobenpe@gmail.com>
2023-12-16 12:33:27 -0300

    dias para navidad

--- CLASE3/3DOM/main.js
+++ CLASE3/3DOM/main.js
@@ -4,8 +4,22 @@ console.log(ahora);
 
 let fecha2 = new Date (2023,11,16)
 
-console.log((ahora.getDay()));
-console.log(ahora.getDate());
-console.log(ahora.getMonth());
+// console.log((ahora.getDay()));
+// console.log(ahora.getDate());
+// console.log(ahora.getMonth());
 
-console.log(fecha2);
+// console.log(fecha2);
+
+
+let casiNavidad = new Date ( 2023,11,24,23,59,59)
+
+let milisParanavidad = casiNavidad-ahora
+
+console.log(milisParanavidad);
+
+let milisPorDia = 24*60*60*1000
+
+console.log(milisPorDia);
+
+let diasParaNavidad = milisParanavidad / milisPorDia
+console.log(diasParaNavidad);
