e30dfcc16f83b445af286d8aae0244cb10c9e9dc
Nicolas <nicobenpe@gmail.com>
2023-12-16 12:19:18 -0300

    segundo dom

---
+++ CLASE3/2DOM/main.js
@@ -0,0 +1,8 @@
+let titulo = document.getElementById("titulo")
+
+titulo.className = "fondo_azul letra_grande_blanca"
+
+let fruta = document.getElementsByClassName("fruta")
+console.log(fruta);
+
+fruta[2].className = "estilo_fruta"
---
+++ CLASE3/3DOM/main.js
@@ -0,0 +1,5 @@
+let ahora = new Date ()
+
+console.log(ahora);
+
+console.log(typeof ahora);
