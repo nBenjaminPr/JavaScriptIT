30bab4a171ec11152cd9195cdb8d9d818db6f4de
Nicolas <nicobenpe@gmail.com>
2024-01-06 12:28:57 -0300

    typeof

---
+++ CLASE4/TEORIA/cuarta/main.js
@@ -0,0 +1,17 @@
+function suma(a,b) {
+    let resultado = a+b
+    return resultado;
+}
+
+function resta(a,b) {
+    let resultado = a-b
+    return resultado;
+}
+
+function multi(a,b) {
+    let resultado = a*b
+    return resultado;
+}
+
+console.log(typeof suma ("oso", 2));
+console.log(suma ("oso", 2));
