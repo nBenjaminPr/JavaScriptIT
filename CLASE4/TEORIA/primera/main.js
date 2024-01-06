e666cfb36b5a29dace9f6d3fc2ae8b1d3dc67b43
Nicolas <nicobenpe@gmail.com>
2024-01-06 11:26:41 -0300

    funciones

---
+++ CLASE4/TEORIA/primera/main.js
@@ -0,0 +1,11 @@
+function saludo() {
+    console.log("Hola, ¿Cómo Estás?");
+}
+
+saludo();
+
+function llamando(nombre) {
+    console.log(`hola!! como estas? me llamo ${nombre}`);
+}
+
+llamando('Juan');
